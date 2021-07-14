1670ff4376389de277c69fbcd6194bfa0b3dbe2f
paodealho404 <phdbnpedro@gmail.com>
2021-07-14 12:43:50 -0300

    Q1

---
+++ Q1A.R
@@ -0,0 +1,7 @@
+x <- c(48, 58, 56, 63, 52, 50, 59, 51, 59, 38, 57, 56, 73, 61, 41, 55, 49, 61, 49, 49, 52, 55, 60, 52, 54, 57, 47, 66, 60, 53, 59, 50, 45, 57, 64, 56, 57, 60, 47, 58, 53, 58, 66, 47, 40);
+tabela <- table(x);
+hist(tabela, main="Histograma da Frequência dos Números", xlab="Frequência", ylab="Números");
+
+
+
+
