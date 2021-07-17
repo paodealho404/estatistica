b8ed1e755b3b778b318c042c1201d2f234abe70e
Lucas <lucaslcf2001@hotmail.com>
2021-07-16 21:07:24 -0300

    Q2 atualizado

--- Q1.R
+++ Q1.R
@@ -1,13 +1,18 @@
-x <- c(48, 58, 56, 63, 52, 50, 59, 51, 59, 38, 57, 56, 73, 61, 41, 55, 49, 61, 49, 49, 52, 55, 60, 52, 54, 57, 47, 66, 60, 53, 59, 50, 45, 57, 64, 56, 57, 60, 47, 58, 53, 58, 66, 47, 40); # cria a tabela com os números
-tabela <- table(x); # separa a frequência de cada número da lista
-tabela
-hist(tabela)
-hist(x, col=c("cyan", "red"), main="Histograma da Frequência dos Números", ylab="Frequência", xlab="Números") # quantidade de números x frequência 
+x <- c(48, 58, 56, 63, 52, 50, 59, 51, 59, 38, 57, 56, 73, 61, 41, 55, 49, 61, 49, 49, 52, 55, 60, 52, 54, 57, 47, 66, 60, 53, 59, 50, 45, 57, 64, 56, 57, 60, 47, 58, 53, 58, 66, 47, 40); # cria a tabela com os n?meros
+
+par(mfrow=c(1, 1))
+
+hist(x, col=c("#9999ff", "#0000ff"), 
+     main="Histograma da FrequÃªncia dos NÃºmeros", 
+     ylab="FrequÃªncia", xlab="NÃºmeros", 
+     ylim=c(0, 20), xlim = c(35, 75), 
+     xaxp = c(30, 80, 10)
+     ) # quantidade de n?meros x frequ?ncia 
 media <- mean(x)
 mediana <- median(x)
 plot(tabela)
 
 par(mfrow=c(1, 2))
 
-boxplot(x, main="Lista de números", ylab="Números", col=c("#ff3333"), border=c("#00001a"))
-boxplot(x, horizontal=TRUE, main="Lista de números", xlab="Números", col=c("#ff3333"), border=c("#00001a"))
+boxplot(x, main="Lista de nÃºmeros", ylab="NÃºmeros", col=c("#ff3333"), border=c("#00001a"))
+boxplot(x, horizontal=TRUE, main="Lista de nÃºmeros", xlab="NÃºmeros", col=c("#ff3333"), border=c("#00001a"))
--- Q2.R
+++ Q2.R
@@ -1,15 +1,23 @@
 dados.pacientes <- read.csv('data/dadosPacientes2021.csv')
+# Tratamento de dados
+
+idade <- dados.pacientes$idade
+dados.pacientes <- subset(dados.pacientes, idade > 0 & idade < 150)
+
+dados.pacientes[dados.pacientes == "Mascuino"] <- "Masculino"
+
+# Fim do tratamento de dados
+
+# Plotando o gráfico da situação atual dos pacientes
 
 situacao.atual.pacientes <- dados.pacientes$situacao_atual
-situacao.atual.pacientes
 
 tabela.situacao.atual <- table(situacao.atual.pacientes)
 tabela.situacao.atual <- sort(tabela.situacao.atual, decreasing = FALSE)
-tabela.situacao.atual
 
 cores <- sample(8:1)
 categorias <- names(tabela.situacao.atual)
-# Plotando o gráfico da situação atual dos pacientes
+
 barplot(tabela.situacao.atual, main="Situação atual dos pacientes",
         col=cores,
         names.arg = c(""),
@@ -18,27 +26,72 @@ barplot(tabela.situacao.atual, main="Situação atual dos pacientes",
         ylab="Números de pacientes",
         )
 legend("topleft", bty="n", 
-       legend=categorias,
-       fill=cores)
+        legend=categorias,
+        fill=cores)
+
+# Fim do gráfico da situação atual dos pacientes
 
 # Plotando o gráfico de óbitos por município
-pacientes.obitos <- subset(dados.pacientes, situacao.atual.pacientes == "Óbito")
+
+pacientes.obitos <- subset(dados.pacientes, dados.pacientes$situacao_atual == "Óbito")
 tabela.pacientes.obitos.municipio <- sort(table(pacientes.obitos$municipio_residencia), decreasing=TRUE)
-barplot(tabela.pacientes.obitos, 
+barplot(tabela.pacientes.obitos.municipio, 
         main="Número de óbitos por município",
         col=c("red", "black"),
         cex.names=0.7,
         xlab="Óbitos",
         ylab="Cidades",
-        las=2, cex.lab=0.1, ylim=c(0,2500),
+        las=2, cex.lab=0.1, ylim=c(0, 2500)
         )
 
+# FIm do gráfico óbitos por município
+
+# Óbitos por sexo e idade
+
+par(mfrow=c(1, 1))
+
+pacientes.masculino <- subset(pacientes.obitos, pacientes.obitos$sexo == "Masculino")
+paciente.masculino.idade <- sort(pacientes.masculino$idade)
+
+# Histograma dos óbitos de homens por idade
+hist(paciente.masculino.idade, 
+        main="Óbitos de pacientes do sexo masculino por idade",
+        ylab="Óbitos",
+        xlab="Idade",
+        xaxp=c(0, 120,12),
+        ylim=c(0, 1000),
+        breaks=10,
+        col=c("#00cc00")
+)
+
+pacientes.feminino <- subset(pacientes.obitos, pacientes.obitos$sexo == "Feminino")
+paciente.feminino.idade <- sort(pacientes.feminino$idade)
+
+# Histograma dos óbitos de feminimo por idade 
+hist(paciente.feminino.idade, 
+        main="Óbitos de pacientes do sexo feminino por idade",
+        ylab="Óbitos",
+        xlab="Idade",
+        xaxp=c(0, 120,12),
+        ylim=c(0, 1000),
+        breaks=10,
+        col=c("#cc0000")
+)
+
+# Fim do gráfico dos óbitos por sexo e idade
+
 # Histograma de óbitos por idade
-pacientes.idade.correta <- subset(pacientes.obitos, pacientes.obitos$idade > 0)
-pacientes.obito.idade <- pacientes.idade.correta$idade
-hist(pacientes.obito.idade, 
+
+idadeMinima <- min(pacientes.obitos$idade); idadeMaxima <- max(pacientes.obitos$idade)
+hist(pacientes.obitos$idade, 
      main="Frequência de óbitos por idade", 
      ylab="Óbitos",
      xlab="Idade",
-     col=c("#99ff99", "#006600") 
+     col=c("#99ff99", "#006600"),
+     ylim=c(0, 1500),
+     xaxp=c(0, 120,12)
      )
+
+# Fim gráfico de óbitos por idade
+
+# Gráfico de óbitos por mês
